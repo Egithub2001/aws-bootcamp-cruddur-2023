e62be0ae4daec3679a9251d03a010703384555c7
Egithub2001 <elam.ce19@gmail.com>
2023-03-25 15:39:49 +0000

    implemented dynamodb into application-part3

--- backend-flask/db/seed.sql
+++ backend-flask/db/seed.sql
@@ -1,13 +1,14 @@
 -- this file was manually created
 INSERT INTO public.users (display_name, email, handle, cognito_user_id)
 VALUES
-  ('Andrew Brown', 'elham.ce@gmail.com','andrewbrown' ,'MOCK'),
-  ('Andrew Bayko','elham.ce@gmail.com','bayko' ,'MOCK');
+  ('elm elm','elam.ce19@gmail.com' , 'elm' ,'MOCK'),
+  ('Andrew Bayko','elham.ce@gmail.com' , 'bayko' ,'MOCK'),
+  ('Londo Mollari','lmollari@centari.com' ,'londo' ,'MOCK');
 
 INSERT INTO public.activities (user_uuid, message, expires_at)
 VALUES
   (
-    (SELECT uuid from public.users WHERE users.handle = 'andrewbrown' LIMIT 1),
+    (SELECT uuid from public.users WHERE users.handle = 'elm' LIMIT 1),
     'This was imported as seed data!',
     current_timestamp + interval '10 day'
   )
